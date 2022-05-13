3e343e743774d2c8b057a93002f30ace7a89a878
unknown <thedien7000@gmail.com>
2022-05-13 12:36:59 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("It was a (adj), cold November day. I woke up to the (adj) smell of (bird type) roasting in the (room) ")
+print("downstairs. I (past_verb) down the stairs to see if I could help (verb) the dinner. My mom said 'See if (name)")
+print("needs a fresh (noun).' So I carried a tray of glasses full of (liquid) into the (ing verb) room. When I got ")
+print("there. I couldn't believe my (body part)! There were (plural noun) (ing verb) on the (noun)!\n")
+
+print("Fill in the blanks -----> ")
+
+adj_1 = input("Enter a adjective: ")
+adj_2 = input("Enter a adjective: ")
+bird_type = input("Enter a type of bird: ")
+room = input("Enter a room in the house: ")
+past_verb = input("Enter a past tense verb: ")
+verb = input("Enter a verb: ")
+relative_name = input("Enter a name of a relative: ")
+noun = input("Enter a noun: ")
+liquid = input("Enter a liquid: ")
+ing_verb = input("Enter a verb ending in ing: ")
+body_part = input("Enter a body part: ")
+plural_noun = input("Enter a plural noun: ")
+ing_verb_2 = input("Enter a verb ending in ing: ")
+noun_2 = input("Enter a noun: ")
+print(f"It was a {adj_1}, cold November day. I woke up to the {adj_2} smell of {bird_type} roasting in the {room} \
+downstairs. I {past_verb} down the stairs to see I could help {verb} the dinner. My mom said,\
+'See if {relative_name} needs a fresh {noun_2}.' So I carried a tray of glasses full of {liquid} into\
+the {ing_verb} room. When I got there, I couldn't believe my {body_part}! There were {plural_noun}\
+{ing_verb_2} on the {noun_2}!")
